a10356c4d50661a5e2c341322888ff59f7a5d7c6
Karl Essinger <xkaess22@gmail.com>
2024-08-05 16:39:14 +0200

    Fixed crash due to uninitialized variables

--- hacking_handler.h
+++ hacking_handler.h
@@ -1,15 +1,54 @@
-#ifndef HACKINGHANDLER_H
-#define HACKINGHANDLER_H
-#include"character.h"
-#include"symbol.h"
-#include"word.h"
-#include<string>
-#include"file_manager.h"
-using namespace std;
+#pragma once
+#include "character.h"
+#include "symbol.h"
+#include "word.h"
+#include <string>
+#include "file_manager.h"
+
 class HackingHandler
 {
+public:
+    HackingHandler();
+    ~HackingHandler();
+    void startRound();
+    int checkWord(int wordPos);
+    int checkSymbol(int symbolPos, int row);
+    void save();
+    void load();
+    void removeRandomWord();
+
+
+    Character** getElements();
+    int getWordLength() const;
+    int getAmountOfWords() const;
+    int getRowLength() const;
+    int getNumberOfRows() const;
+    string getCorrectWord() const;
+    int getAttemptsRemaining() const;
+    int* getAttempts();
+    int getArrayLength() const;
+    int getAttemptNumber() const;
+    string getSaveName() const;
+    int getPoints() const;
+    int getRemovedWords() const;
+
+    void setElements(Character** elements);
+    void setWordLength(int wordLength);
+    void setAmountOfWords(int amountOfWords);
+    void setRowLength(int rowLength);
+    void setNumberOfRows(int numberOfRows);
+    void setCorrectWord(string correctWord);
+    void setAttemptsRemaining(int attemptsRemaining);
+    void setAttempts(int* attempts);
+    void setArrayLength(int arrayLength);
+    void setAttemptNumber(int attemptNumber);
+    void setSaveName(string saveName);
+    void setPoints(int points);
+    void setRemovedWords(int removedWords);
+
 private:
-	const string EIGHTWORDS[20] = { 
+	const string EIGHTWORDS[20] =
+    {
 		"ABRIDGED",
 		"ACCEPTED",
 		"ACCURACY",
@@ -29,61 +68,29 @@ private:
 		"DEVIATED",
 		"DIFFUSED",
 		"DIRECTOR",
-		"DUNGEONS" };
-	const string SYMBOLS[30] = { 
+		"DUNGEONS"
+    };
+
+	const string SYMBOLS[30] =
+    {
 		".",",","/","\\","?","|","_","-","+","%",
 		"$","*",":","!","@","\"","'","#","=",";",
-		"`","^","{","}","[","]","(",")","<",">" };
-	Character* *elements;
-	int wordLength;
-	int amountOfWords;
-	int rowLength;
-	int numberOfRows;
-	string correctWord;
-	int attemptsRemaining;
-	string saveName;
-	int *attempts;
-	int arrayLength;
-	int attemptNumber;
-	int points;
-	int removedWords;
-	bool placeWord(const int minPos, const int maxPos);
-public:
-	HackingHandler();
-	~HackingHandler();
-	void startRound();
-	int checkWord(const int wordPos);
-	int checkSymbol(const int symbolPos,const int row);
-	void save();
-	void load();
-	void removeRandomWord();
+		"`","^","{","}","[","]","(",")","<",">"
+    };
 
-	Character** getElements();
-	int getWordLength()const;
-	int getAmountOfWords()const;
-	int getRowLength()const;
-	int getNumberOfRows()const;
-	string getCorrectWord()const;
-	int getAttemptsRemaining()const;
-	int* getAttempts();
-	int getArrayLength()const;
-	int getAttemptNumber()const;
-	string getSaveName()const;
-	int getPoints()const;
-	int getRemovedWords()const;
+	Character** elements { nullptr };
+	int wordLength { 0 };
+	int amountOfWords { 0 };
+	int rowLength { 0 };
+	int numberOfRows { 0 };
+	string correctWord {};
+	int attemptsRemaining { 0 };
+	string saveName {};
+	int* attempts { nullptr };
+	int arrayLength { 0 };
+	int attemptNumber { 0 };
+	int points { 0 };
+	int removedWords { 0 };
 
-	void setElements(Character* *elements);
-	void setWordLength(int wordLength);
-	void setAmountOfWords(int amountOfWords);
-	void setRowLength(int rowLength);
-	void setNumberOfRows(int numberOfRows);
-	void setCorrectWord(string correctWord);
-	void setAttemptsRemaining(int attemptsRemaining);
-	void setAttempts(int* attempts);
-	void setArrayLength(int arrayLength);
-	void setAttemptNumber(int attemptNumber);
-	void setSaveName(string saveName);
-	void setPoints(int points);
-	void setRemovedWords(int removedWords);
+	bool placeWord(int minPos, int maxPos);
 };
-#endif
